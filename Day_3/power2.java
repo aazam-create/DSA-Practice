c62090ffe0ecd9b6c9d8db82e19d2c2d9990054b
aazam-create <tumhara-github-amameaazam@gmail.com>
2026-04-13 23:05:31 +0530

    fibonacci

---
+++ Day_3/power2.java
@@ -0,0 +1,22 @@
+package Day_3;
+
+import java.util.Scanner;
+
+public class power2 {
+    public static int power(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+
+        return a * power(a, b - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter b : ");
+        int b = sc.nextInt();
+        System.out.println(power(a, b));
+    }
+}
