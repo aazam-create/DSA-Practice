e9e92cefe2886c41db6214a64a0d01054d2672cf
aazam-create <tumhara-github-amameaazam@gmail.com>
2026-04-13 22:01:12 +0530

    Day 3

---
+++ Day_3/sum.java
@@ -0,0 +1,19 @@
+package Day_3;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void sum(int n, int s) {
+        if (n == 0) {
+            System.out.println(s);
+            return;
+        }
+        sum(n - 1, s + n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sum(n, 0);
+    }
+}
